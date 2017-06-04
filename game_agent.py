28c54cf117ab323c91b5c8738982479eee961847
gbedell <gavinbedell@gavins-mbp.attlocal.net>
2017-06-04 11:25:54 -0500

    Implemented alpha-beta pruning algorithm

--- game_agent.py
+++ game_agent.py
@@ -3,6 +3,7 @@ test your agent's strength against a set of known agents using tournament.py
 and include the results in your report.
 """
 import random
+import numpy as np
 
 
 class SearchTimeout(Exception):
@@ -296,8 +297,60 @@ class AlphaBetaPlayer(IsolationPlayer):
         """
         self.time_left = time_left
 
-        # TODO: finish this function!
-        raise NotImplementedError
+        # Initialize the best move so that this function returns something
+        # in case the search fails due to timeout
+        best_move = (-1, -1)
+
+        try:
+            # The try/except block will automatically catch the exception
+            # raised when the timer is about to expire.
+            return self.alphabeta(game, self.search_depth)
+
+        except SearchTimeout:
+            pass  # Handle any actions required after timeout as needed
+
+        # Return the best move from the last completed search iteration
+        return best_move
+    
+    def max_value(self, game, depth, alpha, beta):
+        """
+        TODO: description
+        """
+        if self.time_left() < self.TIMER_THRESHOLD:
+            raise SearchTimeout()
+
+        legal_moves = game.get_legal_moves()
+        if not legal_moves or depth == 0:
+            return self.score(game, self)
+
+        value = float("-inf")
+        for move in legal_moves:
+            forecasted_game = game.forecast_move(move)
+            value = max(value, self.min_value(game=forecasted_game, depth=depth-1, alpha=alpha, beta=beta))
+            if value >= beta:
+                return value
+            alpha = max(alpha, value)
+        return value
+
+    def min_value(self, game, depth, alpha, beta):
+        """
+        TODO: description
+        """
+        if self.time_left() < self.TIMER_THRESHOLD:
+            raise SearchTimeout()
+
+        legal_moves = game.get_legal_moves()
+        if not legal_moves or depth == 0:
+            return self.score(game, self)
+        
+        value = float("inf")
+        for move in legal_moves:
+            forecasted_game = game.forecast_move(move)
+            value = min(value, self.max_value(game=forecasted_game, depth=depth-1, alpha=alpha, beta=beta))
+            if value <= alpha:
+                return value
+            beta = min(beta, value)
+        return value
 
     def alphabeta(self, game, depth, alpha=float("-inf"), beta=float("inf")):
         """Implement depth-limited minimax search with alpha-beta pruning as
@@ -348,4 +401,21 @@ class AlphaBetaPlayer(IsolationPlayer):
             raise SearchTimeout()
 
         legal_moves = game.get_legal_moves()
-        return legal_moves[0]
+
+        if not legal_moves:
+            return game.utility(self)
+        
+        if depth == 0:
+            return self.score(game, self)
+
+        moves_dict = dict()
+        for move in legal_moves:
+            forecasted_game = game.forecast_move(move)
+            score = self.min_value(game=forecasted_game, depth=depth-1, alpha=alpha, beta=beta)
+            # Update our dictionary to include move and corresponding score
+            moves_dict[move] = score
+            # Update alpha based on move score
+            alpha = max(alpha, score)
+        
+        # Return the move with the greatest score
+        return max(moves_dict, key=moves_dict.get)  
